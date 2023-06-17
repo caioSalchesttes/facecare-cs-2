7a24d312e9a76bac41a947b9659daad2bed76bae
caioSalchesttes <caiossxdgamer@gmail.com>
2023-06-17 16:29:32 -0300

    Fist commit -  FaceCare

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// Pega o formulário
+const form = document.getElementById('contato');
+
+// Adiciona um ouvinte de evento 'submit' para o formulário
+form.addEventListener('submit', function(event) {
+    // Pega os inputs do formulário
+    const nomeInput = document.getElementById('nome');
+    const emailInput = document.getElementById('email');
+
+    // Verifica se os campos estão vazios
+    if(nomeInput.value === '' || emailInput.value === '') {
+        // Previne o comportamento padrão do formulário (envio)
+        event.preventDefault();
+        // Mostra um alerta se os campos estiverem vazios
+        alert('Por favor, preencha todos os campos do formulário antes de enviar.');
+    }
+});
